2ece534c1adc487539d541fedbdc012d5467e76a
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-05-19 01:02:24 -0300

    AlunoController adicionado

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use App\Aluno;
+
 class AlunoController extends Controller
 {
     /**
@@ -10,13 +13,26 @@ class AlunoController extends Controller
      *
      * @return void
      */
-    public function __construct(){}
+    public function __construct()
+    {
+    }
 
-    public function index(){
+    public function index()
+    {
         return Aluno::all();
     }
-    public function show($aluno){
-        return Aluno::find($aluno);
+    public function show($aluno)
+    {
+        return (Aluno::find($aluno) == NULL) ? 'Aluno não encontrado' : Aluno::find($aluno);
     }
-    
+
+    public function store(Request $request)
+    {
+        try {
+            Aluno::create($request->all());
+            return response()->json(['Mensagem' => 'Cadastro realizado com sucesso!!']);
+        } catch (QueryException $ex) {
+            return response()->json(['Mensagem' => 'Ocorreu um erro durante o cadastro!!']);
+        }
+    }
 }
